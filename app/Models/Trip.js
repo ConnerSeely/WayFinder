0642c44a1d831a53b5fdaeb631249589401dc030
Conner Seely <Seely.Conner@gmail.com>
2022-05-31 17:06:55 -0600

    Modal added

--- app/Models/Trip.js
+++ app/Models/Trip.js
@@ -30,9 +30,33 @@ export class Trip {
             <button class="btn btn-primary" title="add reservation"> + </button>
           </form>
           <textarea onblur="app.tripsController.updateTrip('${this.id}')" class="border-0">${this.notes}</textarea>
-          <button type="button" class="btn btn-danger" onclick="app.tripsController.deleteTrip('${this.id}')">Delete Trip</button>
-          <button type="button" class="btn btn-primary" onclick="app.tripsController.createTrip()">Create Trip</button>
-          <h4 class="text-end">Total :${this.Total} </h4>`
+          <button type="button" class="btn btn-danger" onclick="app.tripsController.deleteTrip('${this.id}')">Delete Trip</button>  
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  Create Trip
+            </button>
+          <h4 class="text-end">Total :${this.Total} </h4>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form class="trip-form">
+                <input type="text" name="name" id="name" placeholder="name">
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary" onclick="app.tripsController.createTrip()" data-bs-dismiss="modal">Create Trip</button>
+            </div>
+          </div>
+        </div>
+      </div>
+          `
+
     }
 
     get Reservations() {
